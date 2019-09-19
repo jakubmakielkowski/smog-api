ea53722c02d98496b52f0da8250b5f2744ac3cfa
jakubmakielkowski <jakubmakielkowski@gmail.com>
2019-09-19 20:33:40 +0200

    Create index

---
+++ utils/database/createIndex.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+
+const MongoConnection = MongoClient.connect(process.env.DATABASE_URL_DEV, {
+	useNewUrlParser: true, useUnifiedTopology: true
+});
+
+// Create stations index
+const createIndex = async () => {
+	const client = await MongoConnection;
+	const db = client.db(process.env.DATABASE_NAME);
+	
+	db.collection(process.env.DATABASE_COL_STATIONS).createIndex({
+		"address.city": "text",
+		"address.street": "text"
+	});
+	
+	console.log("Stations text index created");
+}
+
+createIndex();
