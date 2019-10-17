dbbdfdb6bc9b10b27fa1f1995cf1ecb3fb03b0f7
jakubmakielkowski <jakubmakielkowski@gmail.com>
2019-10-17 23:51:02 +0200

    Set Access Control

--- server.mjs
+++ server.mjs
@@ -9,9 +9,9 @@ dotenv.config();
 const app = express();
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL_DEV);
-	res.setHeader('Access-Control-Allow-Methods', 'GET');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+	res.setHeader('Access-Control-Allow-Origin', "*");
+	res.setHeader('Access-Control-Allow-Methods', "*");
+	res.setHeader('Access-Control-Allow-Headers', "*");
 	res.setHeader('Access-Control-Allow-Credentials', true);
 	next();
 });
