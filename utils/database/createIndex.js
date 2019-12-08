4b77d4de0ef86bbff851850d799b61bc2f5923bc
jakubmakielkowski <jakubmakielkowski@gmail.com>
2019-12-08 23:16:36 +0100

    Fix createIndex

--- utils/database/createIndex.js
+++ utils/database/createIndex.js
@@ -7,10 +7,10 @@ const createIndex = async () => {
   const client = await MongoConnection;
   const db = client.db(process.env.DATABASE_NAME);
 
-  db.collection(process.env.DATABASE_COL_STATIONS).createIndex({
+  await db.collection(process.env.DATABASE_COL_STATIONS).createIndex({
     'address.city': 'text',
     'address.street': 'text',
-  });
+  })
 
   console.log('Stations text index created');
   process.exit();
